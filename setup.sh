de51e78756b72d9f3b03ae1fc80587d544a538ec
Oscar Sánchez de Zulueta <osadezu@gmail.com>
2022-12-29 17:17:51 -0500

    Change to Java 11

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 echo "Install Jenkins stable release"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
+yum install -y java-11-openjdk
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://jenkins.io/redhat/jenkins-ci.org.key
 yum install -y jenkins
 chkconfig jenkins on
 service jenkins start
 
-echo "Journalctl debug step"
-journalctl -xe
-echo "Systemctl debug step"
-systemctl status jenkins.service
+# echo "Journalctl debug step"
+# journalctl -xe
+# echo "Systemctl debug step"
+# systemctl status jenkins.service
