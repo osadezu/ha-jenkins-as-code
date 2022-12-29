4d19d4ae87111109f7817650430ed10c6e3497a5
Oscar Sánchez de Zulueta <osadezu@gmail.com>
2022-12-29 18:00:48 -0500

    Install Java from amazon extras

--- setup.sh
+++ setup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 echo "Install Jenkins stable release"
-yum remove -y java
-yum install -y java-11-openjdk
+amazon-linux-extras install java-openjdk11
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://jenkins.io/redhat/jenkins-ci.org.key
 yum install -y jenkins
 chkconfig jenkins on
 service jenkins start
-
-# echo "Journalctl debug step"
-# journalctl -xe
-# echo "Systemctl debug step"
-# systemctl status jenkins.service
